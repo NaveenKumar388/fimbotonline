c9d44641c9395d2c204f5be4d6270c2f9094af1a
NAVEENKUMAR <143954407+NaveenKumar388@users.noreply.github.com>
2024-12-02 21:21:48 +0530

    Update db.ts

--- src/db.ts
+++ src/db.ts
@@ -1,5 +1,5 @@
-import { Pool } from 'pg';
-import { redisClient } from './redis.js';
+import pkg from 'pg';
+const { Pool } = pkg;
 
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
